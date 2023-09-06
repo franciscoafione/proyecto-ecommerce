af357929b28dd54d1c579d5b33d1127ff6fab6de
francisco afione <francisco.afione@gmail.com>
2023-09-06 18:18:13 -0300

    Intento recopilar los datos atravez de la api Firebase.

--- scripts/registro.js
+++ scripts/registro.js
@@ -1,6 +1,38 @@
+firebase.initializeApp(firebaseConfig);
+
+
 document.addEventListener('DOMContentLoaded', function () {
     let formulario = document.getElementById("formularioRegistro");
 
+    formulario.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        let nombre = document.getElementById('nombre').value;
+        let fechaNac = document.getElementById('fechaNac').value;
+        let email = document.getElementById('email').value;
+        let usuario = document.getElementById('usuario').value;
+        let contraseña = document.getElementById('contraseña').value;
+
+        const nuevoUsuario = {
+            nombre: nombre,
+            fechaNac: fechaNac,
+            email: email,
+            usuario: usuario,
+            contraseña: contraseña
+        };
+
+        // Guarda el nuevo usuario en Firebase Realtime Database o Firestore
+        firebase.database().ref('PF-Afione').push(nuevoUsuario)
+            .then(() => {
+                console.log('Usuario agregado correctamente.');
+            })
+            .catch(error => console.log(error));
+
+        // Limpia los campos del formulario
+        formulario.reset();
+    });
+
+    /*
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
 
@@ -10,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
         let usuario = document.getElementById('usuario');
         let contraseña = document.getElementById('contraseña');
 
-        const datosUsuario = {
+        const nuevoUsuario = {
             nombre: nombre,
             fechaNac: fechaNac,
             email: email,
@@ -18,6 +50,44 @@ document.addEventListener('DOMContentLoaded', function () {
             contraseña: contraseña
         };
 
+        // Lee el archivo JSON existente
+        fetch('../usuarios.json')
+            .then(response => response.json())
+            .then(data => {
+                // Agrega el nuevo usuario al arreglo existente
+                data.push(nuevoUsuario);
+
+                // Convierte el objeto actualizado a formato JSON
+                const datosJSON = JSON.stringify(data);
+
+                // Guarda los datos actualizados en el archivo JSON existente
+                fetch('../usuarios.json', {
+                    method: 'PUT', // Puedes usar 'POST' o 'PUT' según las configuraciones de tu servidor
+                    body: datosJSON,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                    .then(response => {
+                        if (response.ok) {
+                            console.log('Archivo JSON actualizado correctamente.');
+                        } else {
+                            console.log('Error al actualizar el archivo JSON.');
+                        }
+                    })
+                    .catch(error => console.log(error));
+            })
+            .catch(error => console.log(error));
+
+        // Limpia los campos del formulario
+        formulario.reset();
+    });
+    */
+});
+
+
+
+/*
         fetch("../usuarios.json")
             .then(Response => Response.json())
             .then(data => {
@@ -31,7 +101,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 const blob = new Blob([datosJSON], {type: 'application/json' });
                 const url = URL.createObjectURL(blob);
 
-                // Crea un enlace para descargar el archivo JSON actualizado
+                // Crea un enlace para descargar el archivo JSON actualizado      **********Asi solo descargo un archivo json nuevo.
                 const a = document.createElement('a');
                 a.href = url;
                 a.download = 'formulario.json';
@@ -40,8 +110,4 @@ document.addEventListener('DOMContentLoaded', function () {
             })
 
             .catch(error => console.log(error));
-
-        // Limpia los campos del formulario
-        formulario.reset();
-    });
-});
+*/
